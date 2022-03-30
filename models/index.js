3e8cae3149278728714fe9104ac22b23acf73177
compuwill <wpvorngsam@hotmail.com>
2022-03-29 22:27:57 -0500

    added ability to create and manipulate posts

--- models/index.js
+++ models/index.js
@@ -1,4 +1,16 @@
 const User = require('./User');
 const Post = require("./Post");
 
+// create associations
+
+// one to many, a USER can have many POSTs
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// one to one, one POST can only have ONE user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
