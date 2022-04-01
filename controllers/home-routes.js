5c8f5efd0c5fc04abc6efafe5b2c6433481cb0d2
Will V <wpvorngsam@hotmail.com>
2022-03-31 20:26:30 -0500

    added login and log out session logic

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,9 @@ const { Post, User, Comment } = require('../models');
 
 
 router.get('/', (req, res) => {
+
+    console.log(req.session);
+
     Post.findAll({
       attributes: [
         'id',
@@ -38,4 +41,15 @@ router.get('/', (req, res) => {
       });
   });
 
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+
+
+  
 module.exports = router;
