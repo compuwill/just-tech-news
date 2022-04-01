bf1821009e8f2762d6ad365f1578371797782963
Will V <wpvorngsam@hotmail.com>
2022-03-31 22:14:30 -0500

    cleaned up code and added handlebars helpers

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const helpers = require('./utils/helpers');
 
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
